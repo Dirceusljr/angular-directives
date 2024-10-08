b2473a65ef091582806b15a7d5d9948923f81110
dirceusljr <dirceusljr@gmail.com>
2024-10-07 22:07:24 -0300

    refactor: :recycle: refactor: using inject(ElementRef)

--- src/app/safe-link.directive.ts
+++ src/app/safe-link.directive.ts
@@ -1,28 +1,29 @@
-import { Directive, input } from '@angular/core';
+import { Directive, ElementRef, inject, input } from '@angular/core';
 
 @Directive({
   selector: 'a[appSafeLink]',
   standalone: true,
   host: {
-    '(click)': 'onConfirmLeavePage($event)'
-  }
+    '(click)': 'onConfirmLeavePage($event)',
+  },
 })
 export class SafeLinkDirective {
-
   constructor() {
-    console.log('Safe Link Directive is working!')
-   }
-   queryParam = input('my-app', {alias: 'appSafeLink'})
+    console.log('Safe Link Directive is working!');
+  }
+  queryParam = input('my-app', { alias: 'appSafeLink' });
+  private hostElementRef = inject<ElementRef<HTMLAnchorElement>>(ElementRef);
 
-   onConfirmLeavePage(event: MouseEvent) {
-    const wantsToLeave = window.confirm('Do you want to leave the app?')
+  onConfirmLeavePage(event: MouseEvent) {
+    const wantsToLeave = window.confirm('Do you want to leave the app?');
 
     if (wantsToLeave) {
-      const address = (event.target as HTMLAnchorElement).href;
-      (event.target as HTMLAnchorElement).href = address + '?from=' + this.queryParam()
+      const address = this.hostElementRef.nativeElement.href;
+      this.hostElementRef.nativeElement.href =
+        address + '?from=' + this.queryParam();
       return;
     }
 
-    event.preventDefault()
-   }
+    event.preventDefault();
+  }
 }
